16647cf2d13ab3f942d858cb07f27626d130fecc
mpdev25 <michaelpeake123@gmail.com>
2025-05-11 11:56:43 -0700

    A: word count check

---
+++ main.py
@@ -0,0 +1,25 @@
+def get_book_text(t):
+    with open(t) as f:
+        file_contents = f.read()
+    return file_contents
+def words(file_contents):
+    
+    num_words = len(file_contents.split())
+    
+    return num_words
+    
+def main():
+    
+    book = get_book_text("books/frankenstein.txt")
+    
+    count = words(book)
+    
+    print(f"{count} words found in the document")
+main()
+
+
+
+
+
+
+
